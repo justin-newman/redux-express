86fde84cb4bf636bdfe9e65e16a46cda4e0d45e8
Justin Newman <jnewman@cmnhospitals.org>
2017-05-30 18:27:43 -0600

    first commit

---
+++ client/src/components/NavBar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import '../styles/navbar.css';
+
+const links = [
+    { name: 'Home', path: '/' },
+]
+
+const NavBar = () => (
+    <nav>
+        {links.map(l => <NavLink key={l.name} exact activeClassName="active-link" to={l.path}>{l.name}</NavLink>)}
+    </nav>
+)
+
+export default NavBar;
---
+++ client/src/components/NoMatch.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const NoMatch = () => (
+    <div className="container">
+        <h4>This Is Not The Page You Are Looking For.</h4>
+    </div>
+)
+
+export default NoMatch;
